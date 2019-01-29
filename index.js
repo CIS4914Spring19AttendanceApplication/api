28a3972d29aeb5ab933999ccd811cb79927b4579
Nihir Patel <nihirpatel@ufl.edu>
2019-01-29 17:14:22 -0500

    API protection

--- index.js
+++ index.js
@@ -2,17 +2,39 @@ let express = require('express');
 let http = require('http');
 let app = express();
 let mongoose = require('mongoose');
+let cors = require('cors');
+let jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 var port = process.env.PORT || 8080;
 
+
 app.use(express.json());
+app.use(cors());
+
 
 let eventRouter = require("./routes/event-route");
 let userRouter = require("./routes/user-route");
 let orgRouter = require("./routes/organization-route");
 
 
+const authCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 15,
+        jwksUri: "https://rollcall-app.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://rollcall-api.herokuapp.com',
+    issuer: "https://rollcall-app.auth0.com/",
+    algorithms: ['RS256']
+});
+
+
 if (process.env.NODE_ENV !== 'production') {
     var dotenv = require('dotenv').load();
+}
+else {
+    app.use(authCheck);
 }
 
 mongoose.connect('mongodb://seniorproject:' + process.env.MONGO_PWD + '@ds253324.mlab.com:53324/heroku_r3cbzbjn', {useNewUrlParser: true});
@@ -20,7 +42,6 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 });
-
 
 app.get('/', (req, res) => res.send(''));
 app.listen(port, function () {
