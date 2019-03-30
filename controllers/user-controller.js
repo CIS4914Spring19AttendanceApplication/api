3be00311be9c9d93fb086b2a444f1df5957084fc
samielynne <samieleboli@gmail.com>
2019-03-30 18:47:44 -0400

    added point calculation

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 var User = require("../models/user-model");
 var CheckIn = require("../models/checkin-model");
 var Event = require("../models/event-model");
+var Organization = require("../models/organization-model");
 var _ = require('lodash');
 
 exports.getUserEnrollments = function(req, res) {
@@ -122,7 +123,7 @@ exports.getCheckInHistory = function(req, res, next){
         next();
       }
       else{
-        res.status(404).json({message: "Error Retrieving Organizations"});
+        res.status(404).json({message: "Error Retrieving CheckIns"});
       }
     })
     .catch(err => {
@@ -131,7 +132,7 @@ exports.getCheckInHistory = function(req, res, next){
     });
 };
 
-exports.getEventHistory = function(req, res){
+exports.getEventHistory = function(req, res, next){
   var events = [];
   //loop through all of the check ins to save the events
   res.locals.checkIns.forEach(function(ci){
@@ -142,8 +143,14 @@ exports.getEventHistory = function(req, res){
         //if all of the events are added
         if(events.length == res.locals.checkIns.length){
           //sort the events by org_name
-          events.sort((a, b) => a.org_name.localeCompare(b.org_name));
-          res.status(200).json(events);
+          var result = _.chain(events)
+            .groupBy("org_name")
+            .toPairs()
+            .map(item => _.zipObject(["org", "events"], item))
+            .value();
+
+          res.locals.events = result;
+          next();
         }
       })
       .catch(err => {
@@ -151,4 +158,73 @@ exports.getEventHistory = function(req, res){
         res.status(400).json(err.message);
       });
   });
+};
+
+exports.getPointReqs = function(req, res, next){
+  var result = [];
+
+  res.locals.events.forEach(function(e){
+    var total_needed = 0;
+    Organization.find({"name": e["org"]})
+    .select({'point_categories' : 1, '_id' : 0})
+    .then(document => {
+      if(document){
+        var org = JSON.parse(JSON.stringify(e));
+        org.point_status = [];
+
+        console.log(document[0].point_categories);
+        document[0].point_categories.forEach(function(pc){
+          total_needed = total_needed + pc["points"];
+          org.point_status.push({
+            category: pc["name"],
+            total_points: pc["points"],
+            current_points: 0
+          });
+        });
+
+        //add final json object for total points
+        org.point_status.push({
+          category: "Complete",
+          total_points: total_needed,
+          current_points: 0
+        });
+
+        result.push(org);
+
+        if(result.length == res.locals.events.length){
+          res.locals.points = result;
+          next();
+        }
+      }
+      else{
+        res.status(404).json({message: "Error Retrieving Organization"});
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).json(err.message);
+    });
+  });
+  
+};
+
+exports.getPointHistory = function(req, res){
+  //loop through the events
+  res.locals.points.forEach(function(org){
+    var org_total = 0;
+    org["events"].forEach(function(e){
+      e["point_categories"].forEach(function(p){
+        org["point_status"].forEach(function(s){
+          if(s["category"] == p["name"]){
+            var pointCat = JSON.parse(JSON.stringify(s));
+            var curr = pointCat.current_points + p["points"];
+            s["current_points"] = curr;
+            org_total = org_total + p["points"];
+          }
+        });
+      });
+    });
+    org.point_status[(org["point_status"].length - 1)].current_points = org_total;
+  });
+  res.status(200).json(res.locals.points);
 };
--- routes/user-route.js
+++ routes/user-route.js
@@ -6,7 +6,7 @@ router.post('/registeruser', user_controller.registerUser);
 router.post('/update', user_controller.updateUser);
 router.get('/onboardcheck/:email', user_controller.onboardCheck);
 router.get('/get/:email', user_controller.getUserProfile);
-router.get('/history/:email', user_controller.getCheckInHistory, user_controller.getEventHistory);
+router.get('/history/:email', user_controller.getCheckInHistory, user_controller.getEventHistory, user_controller.getPointReqs, user_controller.getPointHistory);
 router.get('/get/enrollments/:email', user_controller.getUserEnrollments);
 router.get('/get/boardenrollments/:email', user_controller.getUserBoardEnrollments);
 
